bd8151d9cc69b0f9cf8a414b2474f8b580ba79c3
AkshithaGella <akshithagella93@gmail.com>
2026-05-13 11:24:17 +0530

    js

---
+++ javascript/day1/day1/script.js
@@ -0,0 +1,24 @@
+// var num1=10;
+// var num2=20;
+// if (num1>num2){
+//     console.log('hello')
+// }else{
+//     console.log('hey')
+// }
+
+
+// var num=10;
+// if (num%2==0){
+//     console.log('even')
+// }else{
+//     console.log('not even')
+// }
+
+
+var num=2023
+if (num%4==0 & num%100!=0 | num%400==0){
+    console.log('leap')
+}else{
+    console.log('not leap')
+}
+
