0f5f7f9722f9e472850eb992309196bcb8223638
AkshithaGella <akshithagella93@gmail.com>
2026-05-14 11:40:48 +0530

    day2

---
+++ javascript/day2/script.js
@@ -0,0 +1,43 @@
+// generate 5 numbers using for loop
+// for(i=0; i<5; i++){
+//     console.log(i)
+// }
+
+// prime numbers
+
+num=125
+fc=0
+for(i=2; i<num; i++){
+    if (i%num==0){
+        fc+=1
+    }
+}if (fc==0){
+    console.log('prime')
+}else{
+    console.log('not a prime')
+}
+
+
+//palindrome
+ var num=121
+ var rev=0
+ var temp=num
+while (num>0){
+    rem=num%10
+    rev=rev*10+rem
+    num=Math.floor(num/10)
+
+}if (temp==rev){
+    console.log('p')
+}else{
+    console.log('np')
+}
+
+var num=125
+function is_prime(num){
+    fc=0
+    for(i=2; i<num; i++){
+        
+    }
+    
+}
